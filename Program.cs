6dd8072682151053c1e54f4e7f5649032ea51f59
喜欢吃胖头鱼🐟 <chenjc2@g-bits.com>
2024-10-14 13:30:25 +0800

    优化

--- Program.cs
+++ Program.cs
@@ -7,78 +7,87 @@ string binParent = App.GetRealPath( args[ 2 ] );
 bool.TryParse( args[ 3 ], out bool raw );
 
 
+// 输出Debug包，Unity工程调用的时候才能正常显示日志哦
 Console.WriteLine( "param1: " + url );
 Console.WriteLine( "param2: " + csParent );
 Console.WriteLine( "param3: " + binParent );
 Console.WriteLine( "param4: " + raw );
-if ( url.ToLower().EndsWith( ".xlsx" ) )
+try
 {
-    if ( File.Exists( url ) )
+    if ( url.ToLower().EndsWith( ".xlsx" ) )
     {
-        XlsxLoader loader = new( url );
-        ByteArrayExporter exporter = new( loader, binParent, raw );
-        //导出CS声明文件
-        if ( !raw )
+        if ( File.Exists( url ) )
         {
-            IOutScriptHandler outScriptHandler = new Gen_CS_Define_Class( exporter, csParent );
-            outScriptHandler.Apply();
+            XlsxLoader loader = new( url );
+            ByteArrayExporter exporter = new( loader, binParent, raw );
+            //导出CS声明文件
+            if ( !raw )
+            {
+                IOutScriptHandler outScriptHandler = new Gen_CS_Define_Class( exporter, csParent );
+                outScriptHandler.Apply();
+            }
+
+            //导出Ts声明文件.... 其它类型
+            //导出完成
+            Console.WriteLine( $"{loader.dataTableName} export finish." );
+        }
+        else
+        {
+            Console.WriteLine( $"Wrong path: {url}" );
+        }
+    }
+    else if ( Directory.Exists( url ) )
+    {
+        string[] xlsxs = Directory.GetFiles( url, "*.xlsx" );
+        List<Task> tasks = new List<Task>();
+
+        void Convert( string xlsx, bool raw )
+        {
+            XlsxLoader loader = new( xlsx );
+            ByteArrayExporter exporter = new( loader, binParent, raw );
+            //导出CS声明文件
+            if ( !raw )
+            {
+                IOutScriptHandler outScriptHandler = new Gen_CS_Define_Class( exporter, csParent );
+                outScriptHandler.Apply();
+            }
+            //导出Ts声明文件.... 其它类型
+            Console.WriteLine( $"{loader.dataTableName} export finish." );
         }
 
-        //导出Ts声明文件.... 其它类型
-        //导出完成
-        Console.WriteLine( $"{loader.dataTableName} export finish." );
+        //忽略列表
+        List<string> rawfiles = new List<string>();
+        for ( int i = 3; i < args.Length; i++ )
+        {
+            rawfiles.Add( args[ i ] );
+        }
+
+        foreach ( var xlsx in xlsxs )
+        {
+            if ( xlsx.Contains( "~$" ) )
+            {
+                continue;
+            }
+            var xlsxname = Path.GetFileNameWithoutExtension( xlsx );
+            tasks.Add( new Task( ( ) => Convert( xlsx, rawfiles.Contains( xlsxname ) ) ) );
+        }
+
+        foreach ( var task in tasks )
+        {
+            task.Start();
+        }
+
+        Task.WaitAll( tasks.ToArray() );
+
+        Console.WriteLine( $"total xlsx Convert finish. {tasks.Count}" );
     }
     else
     {
-        Console.WriteLine( $"Wrong path: {url}" );
+        Console.WriteLine( $"Wrong path: {url}\nThere are three parameters: the first is the directory or file for xlsx, and the second is the directory for cs script output. The third directory is the output byte stream data file directory." );
     }
+
 }
-else if ( Directory.Exists( url ) )
+catch ( Exception e )
 {
-    string[] xlsxs = Directory.GetFiles( url, "*.xlsx" );
-    List<Task> tasks = new List<Task>();
-
-    void Convert( string xlsx, bool raw )
-    {
-        XlsxLoader loader = new( xlsx );
-        ByteArrayExporter exporter = new( loader, binParent, raw );
-        //导出CS声明文件
-        if ( !raw )
-        {
-            IOutScriptHandler outScriptHandler = new Gen_CS_Define_Class( exporter, csParent );
-            outScriptHandler.Apply();
-        }
-        //导出Ts声明文件.... 其它类型
-        Console.WriteLine( $"{loader.dataTableName} export finish." );
-    }
-
-    //忽略列表
-    List<string> rawfiles = new List<string>();
-    for ( int i = 3; i < args.Length; i++ )
-    {
-        rawfiles.Add( args[ i ] );
-    }
-
-    foreach ( var xlsx in xlsxs )
-    {
-        if ( xlsx.Contains( "~$" ) )
-        {
-            continue;
-        }
-        var xlsxname = Path.GetFileNameWithoutExtension( xlsx );
-        tasks.Add( new Task( ( ) => Convert( xlsx, rawfiles.Contains( xlsxname ) ) ) );
-    }
-
-    foreach ( var task in tasks )
-    {
-        task.Start();
-    }
-
-    Task.WaitAll( tasks.ToArray() );
-
-    Console.WriteLine( $"total xlsx Convert finish. {tasks.Count}" );
-}
-else
-{
-    Console.WriteLine( $"Wrong path: {url}\nThere are three parameters: the first is the directory or file for xlsx, and the second is the directory for cs script output. The third directory is the output byte stream data file directory." );
+    Console.Write( e.ToString() );
 }
